9356b8de3abf3f03ab8c98b5ec57c10065559538
Vikramaditya Mukherjee <vikramadityamukherjee@gmail.com>
2022-08-04 09:39:02 +0530

    Create aws.tf

---
+++ aws.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "web" {
+  ami = "ami-068257025f72f470d"
+  instance_type = "t2.micro"
+  count = "1"
+  subnet_id = "subnet-0d47c7cc17ec8f644"
+  tags = {
+    Name = "Vikram-Web"
+  }
+}
